e81a711668d587786980a0fa88ef1ec2f9e7ae76
Joseph Peterson <joseph.peterson@jhuapl.edu>
2020-08-16 13:33:42 -0400

    Implemented datepicker for date inputs using jquery

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,3 +1,4 @@
+require("jquery-ui")
 require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
@@ -7,9 +8,11 @@ require("channels")
 // Make jQuery available even in the "views" folder
 global.$ = require("jquery")
 
-require("jquery-ui")
-
 $(function(){
 	// Fade Out notices after 5 seconds
 	$('#notice').fadeOut(5000);
+
+	$('body').on('focus', '.date input[type=text]', function(){
+		$(this).datepicker( { dateFormat: 'yy-mm-dd' } );
+	})
 })
